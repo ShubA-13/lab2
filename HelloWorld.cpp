87cddcc274fb3faaa349f7d9b58c4bf803b325c7
ShubA-13 <yura.shubnyj@mail.ru>
2021-03-16 22:43:19 +0300

    commented HelloWorld

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,8 +3,8 @@
 
 int main()
 {
-std::string name;
-std::cin >> name;
-std::cout << "Hello World from " << name << "!";
+std::string name;    //name
+std::cin >> name;    //input
+std::cout << "Hello World from " << name << "!";   //output
 }
 
